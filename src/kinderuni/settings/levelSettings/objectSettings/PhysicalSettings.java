47439cef38ba60e6b8ce83e68fb6440d5926f60b
georg plaz <g.plaz@tripwolf.com>
2015-05-19 12:50:00 +0200

    added some settings files

---
+++ src/kinderuni/settings/levelSettings/objectSettings/PhysicalSettings.java
@@ -0,0 +1,9 @@
+package kinderuni.settings.levelSettings.objectSettings;
+
+import kinderuni.gameLogic.objects.GameObject;
+
+/**
+ * Created by Georg Plaz.
+ */
+public class PhysicalSettings extends GameObjectSettings{
+}
